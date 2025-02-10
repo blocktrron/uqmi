blocktrron/uqmi-qmux

uqmi: enable binding WDS to qmux
Signed-off-by: David Bauer <mail@david-bauer.net>

commands-wds.h
@@ -33,7 +33,10 @@
 	__uqmi_command(wds_set_ip_family, set-ip-family, required, QMI_SERVICE_WDS), \
 	__uqmi_command(wds_set_autoconnect_settings, set-autoconnect, required, QMI_SERVICE_WDS), \
 	__uqmi_command(wds_reset, reset-wds, no, QMI_SERVICE_WDS), \
-	__uqmi_command(wds_get_current_settings, get-current-settings, no, QMI_SERVICE_WDS) \
+	__uqmi_command(wds_get_current_settings, get-current-settings, no, QMI_SERVICE_WDS), \
+	__uqmi_command(wds_bind_mux, bind-mux, no, QMI_SERVICE_WDS), \
+	__uqmi_command(wds_mux_id, mux-id, required, CMD_TYPE_OPTION), \
+	__uqmi_command(wds_ep_iface, ep-iface, required, CMD_TYPE_OPTION) \
 
 
 #define wds_helptext \
@@ -51,4 +54,6 @@
 		"  --set-ip-family <val>:            Set ip-family (ipv4, ipv6, unspecified)\n" \
 		"  --set-autoconnect <val>:          Set automatic connect/reconnect (disabled, enabled, paused)\n" \
 		"  --get-current-settings:           Get current connection settings\n" \
-
+		"  --bind-mux:			     Bind QMUX data port to controller device (use with options below)\n" \
+		"    --mux-id <id>:		     Set QMUX port id\n" \
+		"    --ep-iface <number>:	     Set endpoint interface number\n"

blocktrron/uqmi

uqmi/commands-wds.h
@@ -39,7 +39,10 @@
 	__uqmi_command(wds_modify_profile, modify-profile, required, QMI_SERVICE_WDS), \
 	__uqmi_command(wds_set_pdp_type, pdp-type, required, CMD_TYPE_OPTION), \
 	__uqmi_command(wds_no_roaming, no-roaming, required, CMD_TYPE_OPTION), \
-	__uqmi_command(wds_get_current_settings, get-current-settings, no, QMI_SERVICE_WDS) \
+	__uqmi_command(wds_get_current_settings, get-current-settings, no, QMI_SERVICE_WDS), \
+	__uqmi_command(wds_bind_mux, bind-mux, required, QMI_SERVICE_WDS), \
+	__uqmi_command(wds_ep_type, endpoint-type, required, CMD_TYPE_OPTION), \
+	__uqmi_command(wds_ep_iface, endpoint-iface, required, CMD_TYPE_OPTION) \
 
 
 #define wds_helptext \
@@ -73,4 +76,6 @@
 		"    --auth-type pap|chap|both|none: Use network authentication type\n" \
 		"    --no-roaming false|true         To allow roaming, set to false\n" \
 		"  --get-current-settings:           Get current connection settings\n" \
-
+		"  --bind-mux <id>:		     Bind data session to QMUX multiplex (use with options below)\n" \
+		"    --endpoint-type <type>:	     Set endpoint interface type (hsusb, pcie)\n" \
+		"    --endpoint-iface <number>:	     Set endpoint interface number\n"